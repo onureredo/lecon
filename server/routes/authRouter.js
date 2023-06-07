46801d109549f0907217a788182f7ac82aa047ad
Onur Eredo <onureredo@yahoo.com>
2023-06-07 10:52:23 +0200

    added /me to authRouter

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { signUp, signIn, getUser } from '../controllers/auth.js';
+import verifyToken from '../middlewares/verifyToken.js';
 
 const authRouter = Router();
 
 authRouter.post('/register', signUp);
 authRouter.post('/login', signIn);
+authRouter.get('/me', verifyToken, getUser);
 
 export default authRouter;

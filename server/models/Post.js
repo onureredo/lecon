0fb6ac0ba5877029118b2c712ed5b69747d17bce
Onur Eredo <onureredo@yahoo.com>
2023-06-07 09:55:42 +0200

    post & userSchema

---
+++ server/models/Post.js
@@ -0,0 +1,54 @@
+import mongoose from 'mongoose';
+const { Schema, model } = mongoose;
+
+const postSchema = new Schema(
+  {
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+      maxlength: 200,
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+    reposts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+    parentPost: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Post',
+    },
+    comments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post',
+      },
+    ],
+    hashtags: [
+      {
+        type: String,
+        lowercase: true,
+      },
+    ],
+    mentions: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export default model('Post', postSchema);
--- server/models/User.js
+++ server/models/User.js
@@ -1,62 +1,67 @@
 import mongoose from 'mongoose';
 const { Schema, model } = mongoose;
 
-const userSchema = new Schema({
-  firstName: {
-    type: String,
-    required: [true, 'Name is required'],
-    maxlength: [16, 'Name cannot exceed 16 characters'],
-  },
-  lastName: {
-    type: String,
-    required: [true, 'Lastname is required'],
-    maxlength: [16, 'Name cannot exceed 16 characters'],
-  },
-  username: {
-    type: String,
-    unique: true,
-    required: [true, 'Username is required'],
-    maxlength: [12, 'Username cannot exceed 12 characters'],
-  },
-  email: {
-    type: String,
-    unique: true,
-    required: [true, 'Email is required'],
-  },
-  password: {
-    type: String,
-    required: [true, 'Password is required'],
-    select: false,
-  },
-  bio: String,
-  profileImage: String,
-  bgImage: String,
-  followers: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, 'Name is required'],
+      maxlength: [16, 'Name cannot exceed 16 characters'],
     },
-  ],
-  following: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
+    lastName: {
+      type: String,
+      required: [true, 'Lastname is required'],
+      maxlength: [16, 'Name cannot exceed 16 characters'],
     },
-  ],
-  createdAt: {
-    type: Date,
-    default: Date.now,
+    username: {
+      type: String,
+      unique: true,
+      required: [true, 'Username is required'],
+      maxlength: [12, 'Username cannot exceed 12 characters'],
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: function (v) {
+          return /^([\w-.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(v);
+        },
+        message: (props) => `${props.value} is not a valid email!`,
+      },
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+      select: false,
+    },
+    bio: String,
+    profileImage: String,
+    bgImage: String,
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+    role: {
+      type: String,
+      enum: ['user', 'moderator', 'admin', 'super'],
+      default: 'user',
+      select: false,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
   },
-  role: {
-    type: String,
-    enum: ['user', 'moderator', 'admin', 'super'],
-    default: 'user',
-    select: false,
-  },
-  isVerified: {
-    type: Boolean,
-    default: false,
-  },
-});
+  { timestamps: true }
+);
 
 export default model('User', userSchema);

2053d95b4d7caa0a5770fa9a29ee68f756c39e8f
Onur Eredo <onureredo@yahoo.com>
2023-06-07 10:38:43 +0200

    authController

---
+++ server/controllers/auth.js
@@ -0,0 +1,41 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import asyncHandler from '../utils/asynchHandler.js';
+import ErrorResponse from '../utils/ErrorResponse.js';
+import User from '../models/User.js';
+
+// Register
+export const signUp = asyncHandler(async (req, res, next) => {
+  const { firstName, lastName, username, email, password } = req.body;
+  const existingUser = await User.findOne({ email });
+  if (existingUser)
+    throw new ErrorResponse('An account with this Email already exists.');
+  const hash = await bcrypt.hash(password, 10);
+  const newUser = await User.create({
+    firstName,
+    lastName,
+    username,
+    email,
+    password: hash,
+  });
+  const token = jwt.sign({ uid: newUser._id }, process.env.JWT_SECRET);
+  res.status(201).send({ token });
+});
+
+// Login
+export const signIn = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+  const existingUser = await User.findOne({ email }).select('+password');
+  if (!existingUser) throw new ErrorResponse('User does not exist.', 404);
+  const matchPassword = await bcrypt.compare(password, existingUser.password);
+  if (!matchPassword) throw new ErrorResponse('Password is incorrect', 401);
+
+  const token = jwt.sign({ uid: existingUser._id }, process.env.JWT_SECRET);
+  res.status(200).send({ token });
+});
+
+// Get User
+export const getUser = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.uid);
+  res.json(user);
+});

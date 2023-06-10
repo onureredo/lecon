996064082ca74e655e7c1cde0c4b1d91d8d0c49a
Onur Eredo <onureredo@yahoo.com>
2023-06-10 02:05:27 +0200

    added date-fns

---
+++ client/src/utils/dateFormat.tsx
@@ -0,0 +1,26 @@
+import { format } from 'date-fns';
+
+export function formatTweetDate(date: string): string {
+  const tweetDate = new Date(date);
+  const now = new Date();
+  const diffInMinutes = Math.round(
+    (now.getTime() - tweetDate.getTime()) / (1000 * 60)
+  );
+  const diffInHours = Math.round(diffInMinutes / 60);
+
+  if (diffInMinutes < 1) {
+    return 'Just now';
+  } else if (diffInMinutes < 60) {
+    return `${diffInMinutes}m`;
+  } else if (diffInHours < 24) {
+    return `${diffInHours}h`;
+  } else {
+    return format(tweetDate, 'MMM d');
+  }
+}
+
+export function formatTitleDate(date: string): string {
+  const tweetDate = new Date(date);
+  const formattedTitleDate = format(tweetDate, 'hh:mm a · MMMM d yyyy');
+  return formattedTitleDate;
+}

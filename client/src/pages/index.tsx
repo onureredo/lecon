e28f7c3de7837a0765581f3a6fc0159835173e4c
Onur Eredo <onureredo@yahoo.com>
2023-06-15 17:08:53 +0200

    added vercel analytics

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -2,6 +2,7 @@ import { Footer } from '@/components/Footer';
 import { Header } from '@/components/Header';
 import { Timeline } from '@/components/Timeline';
 import { AuthProvider } from '@/context/AuthContext';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Header />
       <Timeline />
       <Footer />
+      <Analytics />
     </AuthProvider>
   );
 }
